4ce8f6833d90f0c6156d7851c810ac1effc67bf4
wangwei12 <wangwei12@kuaishou.com>
2019-11-14 20:15:35 +0800

    squash

--- textrank/text_rank.py
+++ textrank/text_rank.py
@@ -1,6 +1,10 @@
+from typing import List
+import itertools
 
 
-def extract_keyword(text: str, window_size: int = 5, split:bool = True):
+def extract_keyword(text: str, window_size: int = 5, spliter = jieba_spliter):
+	words = spliter(text)
+
 	pass
 
 
@@ -8,6 +12,9 @@ def extract_summary(text: str, ):
 	pass
 
 
-def sentence_similarity(s1: str, s2: str) -> float:
+def sentence_similarity_(s1: str, s2: str) -> float:
 	pass
 
+
+def jieba_spliter(text: str) -> List[str]:
+	pass
